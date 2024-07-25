7d565fcae9aa0395643895dc1c1d0e90cfeb8469
maliByatzes <malib2027@gmail.com>
2024-07-25 12:34:17 +0200

    connect to mongodb

--- app.ts
+++ app.ts
@@ -5,6 +5,21 @@ const app = new Hono();
 
 app.use("*", logger());
 
+app.post("/upload", async (c) => {
+  try {
+    const body = await c.req.parseBody();
+    let uploadedFile: File;
+    if (body['file'] instanceof File) {
+      uploadedFile = body['file'];
+      console.log(uploadedFile);
+    }
+    return c.text("Upload route");
+  } catch (err: any) {
+    console.error(`Error in upload route: ${err.message}`);
+    return c.json({ error: "Internal Server Error" }, 500);
+  }
+});
+
 app.get('/', (c) => {
   return c.text("Hello Markdown Note App");
 });
