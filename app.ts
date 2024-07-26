26a005f14e0cae808851aadb3dca45b6e325c4a1
maliByatzes <malib2027@gmail.com>
2024-07-26 10:35:48 +0200

    re-organize code into modules

--- app.ts
+++ app.ts
@@ -1,77 +1,14 @@
 import { Hono } from "hono";
-import { logger } from "hono/logger";
-import { FileService } from "./services/file.service";
-import { bodyLimit } from "hono/body-limit";
-import { marked } from "marked";
+import fileRoutes from "./routes/file.routes";
 
 const app = new Hono();
 
-const BODY_LIMIT = 16 * 1024 * 1024;
-
-let fileService: FileService;
-
-app.use("*", logger());
-
-app.use(async (_, next) => {
-  if (!fileService) {
-    fileService = new FileService();
-  }
-  await next();
-});
-
-app.post("/upload", bodyLimit({ maxSize: BODY_LIMIT }), async (c) => {
-  try {
-    const body = await c.req.parseBody();
-
-    let uploadedFile: File;
-    if (body['file'] instanceof File) {
-      uploadedFile = body['file'];
-    } else {
-      return c.json({ error: "Invalid file format" }, 400);
-    }
-
-    if (uploadedFile.type !== "text/markdown") {
-      return c.json({ error: "Invalid file format" }, 400);
-    }
-
-    if (uploadedFile.size > BODY_LIMIT) {
-      return c.json({ error: "Exceeded file size limit" }, 400);
-    }
-
-    const arrayBuffer = await uploadedFile.arrayBuffer();
-    const buffer = Buffer.from(arrayBuffer);
-
-    const fileId = await fileService.saveFile(uploadedFile.name, uploadedFile.type, buffer);
-
-    return c.json({ filedId: fileId.toString() }, 201);
-  } catch (err: any) {
-    console.error(`Error in upload route: ${err.message}`);
-    return c.json({ error: "Internal Server Error" }, 500);
-  }
-});
-
-app.get("/file/:id", async (c) => {
-  const fileId = c.req.param("id");
-
-  try {
-    const file = await fileService.getFile(fileId);
-
-    if (!file) {
-      return c.notFound();
-    }
-
-    const html = marked(file.data.toString());
-
-    c.header('Content-Type', 'text/html')
-    return c.html(html);
-  } catch (err: any) {
-    console.error(`Error in get file route: ${err.message}`);
-    return c.json({ error: "Internal Server Error" }, 500);
-  }
-});
-
-app.get('/', (c) => {
+app.get('/healthhecker', (c) => {
   return c.text("Hello Markdown Note App");
 });
 
+const apiRoutes = app.basePath("/api/v1")
+  .route("/files", fileRoutes);
+
 export default app;
+export type ApiRoutes = typeof apiRoutes; // RPC for frontend
---
+++ routes/file.routes.ts
@@ -0,0 +1,71 @@
+import { Hono } from "hono";
+import { bodyLimit } from "hono/body-limit";
+import { marked } from "marked";
+import { FileService } from "../services/file.service";
+
+const fileRoutes = new Hono();
+
+let fileService: FileService;
+
+fileRoutes.use(async (_, next) => {
+  if (!fileService) {
+    fileService = new FileService();
+  }
+  await next();
+});
+
+const BODY_LIMIT = 16 * 1024 * 1024;
+
+fileRoutes.post("/upload", bodyLimit({ maxSize: BODY_LIMIT }), async (c) => {
+  try {
+    const body = await c.req.parseBody();
+
+    let uploadedFile: File;
+    if (body['file'] instanceof File) {
+      uploadedFile = body['file'];
+    } else {
+      return c.json({ error: "Invalid file format" }, 400);
+    }
+
+    if (uploadedFile.type !== "text/markdown") {
+      return c.json({ error: "Invalid file format" }, 400);
+    }
+
+    if (uploadedFile.size > BODY_LIMIT) {
+      return c.json({ error: "Exceeded file size limit" }, 400);
+    }
+
+    const arrayBuffer = await uploadedFile.arrayBuffer();
+    const buffer = Buffer.from(arrayBuffer);
+
+    const fileId = await fileService.saveFile(uploadedFile.name, uploadedFile.type, buffer);
+
+    return c.json({ filedId: fileId.toString() }, 201);
+  } catch (err: any) {
+    console.error(`Error in upload route: ${err.message}`);
+    return c.json({ error: "Internal Server Error" }, 500);
+  }
+});
+
+fileRoutes.get("/:id", async (c) => {
+  const fileId = c.req.param("id");
+  try {
+    const file = await fileService.getFile(fileId);
+
+    if (!file) {
+      return c.notFound();
+    }
+
+    if (file.data) {
+      let html = await marked(file.data.toString());
+      c.header('Content-Type', 'text/html')
+      return c.html(html);
+    }
+    return c.notFound();
+  } catch (err: any) {
+    console.error(`Error in get file route: ${err.message}`);
+    return c.json({ error: "Internal Server Error" }, 500);
+  }
+});
+
+export default fileRoutes;
